aafca055b6775ccbcd5bcac83980bdc7373db9ca
William0233 <33510404+William0233@users.noreply.github.com>
2022-10-11 13:59:06 -0400

    Add files via upload
    How would you like to have a program that downloads the latest price data for any company you want to invest in? Maybe you are a day trader and looking for a more efficient way to get data into the same excel sheet where you have formulas that automatically evaluate security. Perhaps you are a financial modeler who’s looking for a way to start your model. This program named “MarketPriceDataClass “ will help with downloading market prices into your preferred workbook, it will find the designated sheet and the row columns you want your data to download.  
    MarketPriceDataClass is a program that fetches current and historical data from Finance Yahoo and returns a Panda DataFrame; then it exports into an excel sheet where it may be used for all sorts of applications. This script also can create folders (arranged by dates) to store the downloaded ticker data in a CSV file for future uses.
    First thing is to know that this is easier than you think, especially if you’re not a programmer. Simply follow the instructions laid out, and you shall have an automated system in no time.  
    SETTING UP YOUR ENVIRONMENT:
    Be sure to have Visual Code installed to run your python file.
    Create an excel workbook in the same folder as your Visual Code.
    Install Xlwings as an Add-in excel, which will help python to communicate with excel: https://docs.xlwings.org/en/stable/addin.htmlCopy and paste MarketPriceDataClass python scrip into Visual Code
    RUNNING MarketPriceDataClass PYTHON SCRIP:
    Add your desired ticker in “tickerName”
    Enter the current date of interest in “todayDate”
    Enter the number of data desired in “numData”. (Key in 252 to get data for year-to-date)
    Like magic, your data should be downloaded and printed. If you have Xlwings installed, then you may enter the followings:
    The name of your excel workbook in “workbookName”
    Select the excel sheet to allocate the data “sheetName”
    Clear previous data of a range with “clearCols”
    The row-col you want your initial data point to be allocated in excel.
    I wish you the best in your endeavor.

---
+++ MarketPriceDataClass.py
@@ -0,0 +1,75 @@
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+from pandas_datareader._utils import RemoteDataError
+import xlwings as xw
+
+class MarketPriceData(object):
+
+    def __init__(self, selectedTicker, input_date_string, numData):
+        self.numData = numData
+        numData = numData + (numData/2)
+        self.selectedTicker = selectedTicker
+        self.input_date = datetime.strptime(input_date_string, '%m/%d/%Y')
+        self.start_date = self.input_date - timedelta(days=numData)
+        self.end_date = self.input_date.strftime("%m/%d/%Y")
+        self.start_date = self.start_date.strftime("%m/%d/%Y")
+        self.folder_date = self.input_date.strftime('%Y-%m-%d')
+        dirname = os.path.dirname(__file__)
+        self.folderName = os.path.join(dirname,'Data\\RandomSecurity\\{}_Imports'.format(self.folder_date))
+    
+    def get_workbook(self, workbook_name):
+        xw.Book(os.path.join(os.path.dirname(__file__),workbook_name)).set_mock_caller()
+        wb = xw.Book.caller()
+        return wb
+
+    def get_sheet(self,workbook_name, sheet_name):
+        wb = self.get_workbook(workbook_name)
+        return wb.sheets[sheet_name]
+    
+    @xw.sub
+    def loadSelected(self):
+        #This function dowloads security data from finacance yahoo
+        selected_ticker = self.selectedTicker
+        input_date = self.start_date    
+        end_date = self.end_date
+        folderName = self.folderName
+        os.makedirs(folderName, exist_ok=True)
+        csvFilename = '{}\\{}.csv'.format(folderName,selected_ticker)
+        panel_data = pd.DataFrame()
+        
+        try:
+            from pandas_datareader import data
+            panel_data = data.DataReader(
+                            selected_ticker, 'yahoo', input_date, end_date)
+            panel_data = panel_data[['Open', 'High', 'Low', 'Close', 'Adj Close',
+                                        'Volume']].tail(self.numData)
+            print("Writing CSV File {}.csv".format(selected_ticker))
+            panel_data.to_csv(csvFilename)
+            #panel_data.reset_index(inplace=True)
+        except RemoteDataError:
+            print("No symbol called {}".format(selected_ticker))
+            exit()    
+        return panel_data
+
+
+
+
+tickerName = 'AAPL'
+todayDate ='8/25/2022'
+numData = 252 # one year of trading
+
+data = MarketPriceData(tickerName, todayDate, numData)
+price_panel = data.loadSelected()
+print(price_panel)
+
+## if you have xlwings installed in excel, you may run the next few lines
+## to have the data prints directely in excel
+## if not use the # to comment the next 7 lines
+workbookName ='EngineA7Live.xlsb'
+sheetName = 'Imports'
+clearCols = 'CK1:DM500000'
+initialCols = 'CK1'
+sheet = data.get_sheet(workbookName, sheetName) 
+sheet.range(clearCols).clear_contents()
+sheet.range(initialCols).value = price_panel
